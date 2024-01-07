e4ef27a40059c2b42de565221941be1639ed1007
Edward Heaver <edward.heaver01@gmail.com>
2024-01-07 21:03:39 +0000

    decorator demo code

---
+++ decorator-demo/decorator.py
@@ -0,0 +1,32 @@
+class Person:
+    age:int
+    name:str
+    is_programmer:bool
+
+    def __init__(self, age:int, name:str, i:bool):
+        self.age = age
+        self.name = name
+        self.is_programmer = i
+
+    def log():
+        def log_decorator(func):
+           def wrapper_func(self, thing:str=""):
+                job = "programmer" if self.is_programmer else "not programmer"
+                activity = thing.lower() if len(thing) > 0 else "unknown"
+
+                with open("file.csv", "a") as f:
+                    f.write(f"{self.name},{self.age},{job},{activity}\n")
+                    f.close()
+
+                func(thing)
+           return wrapper_func
+        return log_decorator
+    
+    @log()
+    def do_thing(thing:str):
+        print("DECORATOR DID THE WORK")
+
+
+t = Person(30, "Ed", True)
+
+t.do_thing("Eating dinner")
